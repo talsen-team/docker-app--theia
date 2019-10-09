9dc8fc391a81a7eedf86a2c5542374ae1a67ce1d
David Lechner <david.lechner@talsen.team>
2019-10-09 13:41:02 +0200

    invoke dojo commands

--- docker/server--theia/rootfs/etc/talsen/dojo.bash
+++ docker/server--theia/rootfs/etc/talsen/dojo.bash
@@ -7,12 +7,25 @@ source /etc/talsen/util/detect-script-dir.bash
 DOJO_DIR=$( detect_script_dir ${BASH_SOURCE[0]} )
 
 function print_help() {
-    local COMMAND_DIR="${DOJO_DIR}/command"
+    local COMMAND_DIR=${DOJO_DIR}/command
 
     for COMMAND in $( cd ${COMMAND_DIR} && ls *.bash )
     do
         /bin/bash ${COMMAND_DIR}/${COMMAND}
     done
+}
+
+function invoke_command() {
+    local COMMAND_SCRIPT=${DOJO_DIR}/command/${1}.bash
+
+    if [ ! -f ${COMMAND_SCRIPT} ];
+    then
+        echo "Error: unknown command \"${1}\""
+
+        exit 1
+    fi
+
+    /bin/bash ${COMMAND_SCRIPT} ${@:2}
 }
 
 if [ ${#} = 0 ];
@@ -21,3 +34,5 @@ then
 
     exit 0
 fi
+
+invoke_command ${@:1}
