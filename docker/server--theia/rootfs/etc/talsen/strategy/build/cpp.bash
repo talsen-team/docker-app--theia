bb0e2cd076558a4f9f2beb2318673891e540078f
David Lechner <david.lechner@talsen.team>
2019-10-09 17:22:32 +0200

    implement build execution for cpp

--- docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
+++ docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
@@ -2,4 +2,30 @@
 
 set -euo pipefail
 
-echo "Builing a C++ templated workspace..."
+BUILD_DIR=.build
+PWD=$( pwd )
+TARGET_NAME=run-workspace
+
+export TARGET_NAME
+
+if [ ! -d ${BUILD_DIR} ];
+then
+    mkdir --parents    \
+          ${BUILD_DIR}
+
+    cd ${BUILD_DIR}
+
+    cmake ../src -DCMAKE_BUILD_TYPE=Debug
+else
+    cd ${BUILD_DIR}
+fi
+
+echo ""
+
+make ${TARGET_NAME}
+
+echo ""
+
+./${TARGET_NAME}
+
+cd ${PWD}
