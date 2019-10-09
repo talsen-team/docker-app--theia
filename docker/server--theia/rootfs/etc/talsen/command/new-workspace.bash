046d0a59d20388faa984ec7c40bf4888199989ef
David Lechner <david.lechner@talsen.team>
2019-10-09 13:50:18 +0200

    create new workspace script

--- docker/server--theia/rootfs/etc/talsen/command/new-workspace.bash
+++ docker/server--theia/rootfs/etc/talsen/command/new-workspace.bash
@@ -9,9 +9,9 @@ WORKSPACE_NAME_PREFIX=$( date +%F )
 
 function print_help() {
     echo "Usage: dojo ${SCRIPT_NAME} <name>"
-    echo "Creates a new empty dojo workspace."
     echo "  <name>: Will have the current date prepended, the full name"
     echo "          looks like the following: \"${WORKSPACE_NAME_PREFIX}-<name>\""
+    echo "--> Creates a new empty dojo workspace."
     echo ""
 }
 
@@ -20,4 +20,22 @@ then
     print_help
 
     exit 0
+fi
+
+WORKSPACE_NAME=${1}
+WORKSPACE_DIR=/home/project/${WORKSPACE_NAME_PREFIX}-${WORKSPACE_NAME}
+
+if [[ ${WORKSPACE_NAME} =~ ^[-_0-9a-zA-Z]+$ ]]
+then
+    mkdir --parents        \
+          ${WORKSPACE_DIR}
+
+    echo "--> A new empty workspace has been created at \"${WORKSPACE_DIR}\"."
+    echo "    Open it in theia to proceed."
+
+    exit 0
+else
+    echo "Error: The workspace name \"${WORKSPACE_NAME}\" is invalid"
+
+    exit 1
 fi
