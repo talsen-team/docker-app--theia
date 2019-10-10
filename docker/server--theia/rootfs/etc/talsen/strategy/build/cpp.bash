f7b483a8256391efe9b5fac5541a37d4b8959fe9
David Lechner <david.lechner@talsen.team>
2019-10-10 15:22:03 +0200

    replace hardcoded workspace indicators by bash includes

--- docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
+++ docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
@@ -2,17 +2,20 @@
 
 set -euo pipefail
 
+source /etc/talsen/util/indicator/workspace-gunit-lib-indicator.bash
+source /etc/talsen/util/indicator/workspace-meson-options-indicator.bash
+
 BUILD_DIR=.build
 PWD=$( pwd )
 TARGET_NAME=run-workspace
 
 if [ ! -d ${BUILD_DIR} ];
 then
-    cat .workspace/.meson-options \
+    cat ${WORKSPACE_MESON_OPTIONS_INDICATOR} \
     > meson_options.txt
     echo "option( 'exe_name', type : 'string', value : '${TARGET_NAME}' )" \
     >> meson_options.txt
-    echo "option( 'gunit_lib_dir', type : 'string', value : '$( pwd )/.workspace/.gunit/lib' )" \
+    echo "option( 'gunit_lib_dir', type : 'string', value : '$( pwd )/${WORKSPACE_GUNIT_LIB_INDICATOR}' )" \
     >> meson_options.txt
 
     meson ${BUILD_DIR}
---
+++ docker/server--theia/rootfs/etc/talsen/util/indicator/workspace-gunit-lib-indicator.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /etc/talsen/util/indicator/workspace-indicator.bash
+
+WORKSPACE_GUNIT_LIB_INDICATOR=${WORKSPACE_INDICATOR}/.gunit/lib
