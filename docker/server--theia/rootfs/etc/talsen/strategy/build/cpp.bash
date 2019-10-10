ee9a02e1a09772efa3cc3371f617afc51c6c8e46
David Lechner <david.lechner@talsen.team>
2019-10-10 15:01:14 +0200

    run tests manually instead of via ninja

--- docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
+++ docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
@@ -4,11 +4,18 @@ set -euo pipefail
 
 BUILD_DIR=.build
 PWD=$( pwd )
+TARGET_NAME=run-workspace
 
 if [ ! -d ${BUILD_DIR} ];
 then
-    echo "option( 'gunit_lib_dir', type : 'string', value : '$( pwd )/.workspace/.gunit/lib' )" > meson_options.txt
-    echo "option( 'gunit_include_dir', type : 'string', value : '.workspace/.gunit/lib/include' )" >> meson_options.txt
+    echo "" \
+    > meson_options.txt
+    echo "option( 'exe_name', type : 'string', value : '${TARGET_NAME}' )" \
+    >> meson_options.txt
+    echo "option( 'gunit_lib_dir', type : 'string', value : '$( pwd )/.workspace/.gunit/lib' )" \
+    >> meson_options.txt
+    echo "option( 'gunit_include_dir', type : 'string', value : '.workspace/.gunit/lib/include' )" \
+    >> meson_options.txt
 
     meson ${BUILD_DIR}
 fi
@@ -21,6 +28,6 @@ ninja
 
 echo ""
 
-ninja test
+./${TARGET_NAME}
 
 cd ${PWD}
