5d13b3d1e1a83bd688f18bc40c9c96496b6fa060
David Lechner <david.lechner@talsen.team>
2019-10-10 14:54:49 +0200

    build sources via meson

--- docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
+++ docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
@@ -4,28 +4,23 @@ set -euo pipefail
 
 BUILD_DIR=.build
 PWD=$( pwd )
-TARGET_NAME=run-workspace
-
-export TARGET_NAME
 
 if [ ! -d ${BUILD_DIR} ];
 then
-    mkdir --parents    \
-          ${BUILD_DIR}
+    echo "option( 'gunit_lib_dir', type : 'string', value : '$( pwd )/.workspace/.gunit/lib' )" > meson_options.txt
+    echo "option( 'gunit_include_dir', type : 'string', value : '.workspace/.gunit/lib/include' )" >> meson_options.txt
 
-    cd ${BUILD_DIR}
-
-    cmake ../src -DCMAKE_BUILD_TYPE=Debug
-else
-    cd ${BUILD_DIR}
+    meson ${BUILD_DIR}
 fi
 
-echo ""
-
-make ${TARGET_NAME}
+cd ${BUILD_DIR}
 
 echo ""
 
-./${TARGET_NAME}
+ninja
+
+echo ""
+
+ninja test
 
 cd ${PWD}
