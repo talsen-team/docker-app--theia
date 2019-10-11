4738832005b91343167808d19cf50f2f49679f29
David Lechner <david.lechner@talsen.team>
2019-10-11 16:33:24 +0200

    refine help for focus-on command

--- docker/server--theia/rootfs/etc/talsen/command/focus-on.bash
+++ docker/server--theia/rootfs/etc/talsen/command/focus-on.bash
@@ -8,10 +8,27 @@ source /etc/talsen/util/print-help-flag-text.bash
 
 SCRIPT_NAME=$( detect_command_name ${0} )
 
+FOCI=""
+for FOCUS in $( find /etc/talsen/focus/ -maxdepth 1 -name *.foci -type f -printf '%f ' | xargs echo )
+do
+    FOCI="${FOCI}${FOCUS} "
+done
+FOCI="${FOCI}"
+
 function print_help() {
     echo "Usage: dojo ${SCRIPT_NAME} <focus>"
     echo "  <focus>: The focus to set, supported foci are:"
-    echo "           <none>"
+
+    for FOCUS in ${FOCI}
+    do
+        FOCI_FOR=""
+        while IFS= read -r FOCUS_FOR
+        do
+            FOCI_FOR="${FOCI_FOR}${FOCUS_FOR} "
+        done <<< $( cat /etc/talsen/focus/${FOCUS} )
+    echo "           ${FOCUS/.foci/}: ${FOCI_FOR}"
+    done
+
     print_help_flag_text
     echo "--> Sets a new focus for the workspace build."
 }
