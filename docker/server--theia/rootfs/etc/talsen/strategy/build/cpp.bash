c79d3f748235d27055d39fb69703f1f4dc077c15
David Lechner <david.lechner@talsen.team>
2019-10-11 12:24:48 +0200

    consider cpp flags for build focus implementation

--- docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
+++ docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
@@ -2,13 +2,17 @@
 
 set -euo pipefail
 
+source /etc/talsen/util/indicator/workspace-build-focus-indicator.bash
 source /etc/talsen/util/indicator/workspace-gunit-lib-indicator.bash
 source /etc/talsen/util/indicator/workspace-meson-options-indicator.bash
 source /etc/talsen/util/indicator/workspace-cpp-flags-default-indicator.bash
 
 BUILD_DIR=.build
+BUILD_FOCUS=$( cat ${WORKSPACE_BUILD_FOCUS_INDICATOR} )
 PWD=$( pwd )
 TARGET_NAME=run-workspace
+
+source /etc/talsen/util/indicator/workspace-cpp-flags-${BUILD_FOCUS}-indicator.bash
 
 if [ ! -d ${BUILD_DIR} ];
 then
@@ -17,14 +21,30 @@ then
     do
         CPP_FLAGS_DEFAULT="'${FLAG}', ${CPP_FLAGS_DEFAULT}"
     done
-    CPP_FLAGS_DEFAULT="${CPP_FLAGS_DEFAULT::-2}"
+    if (( ${#CPP_FLAGS_DEFAULT} > 1 ));
+    then
+        CPP_FLAGS_DEFAULT="${CPP_FLAGS_DEFAULT::-2}"
+    fi
+
+    CPP_FLAGS_FOCUS=""
+    for FLAG in $( cat ${WORKSPACE_CPP_FLAGS_FOCUS_INDICATOR} )
+    do
+        CPP_FLAGS_FOCUS="'${FLAG}', ${CPP_FLAGS_FOCUS}"
+    done
+    if (( ${#CPP_FLAGS_FOCUS} > 1 ));
+    then
+        CPP_FLAGS_FOCUS="${CPP_FLAGS_FOCUS::-2}"
+    fi
 
     SOURCES=""
     for SOURCE in $( find . -name *.cpp -printf '%P ' | xargs echo )
     do
         SOURCES="'${SOURCE}', ${SOURCES}"
     done
-    SOURCES="${SOURCES::-2}"
+    if (( ${#SOURCES} > 1 ));
+    then
+        SOURCES="${SOURCES::-2}"
+    fi
 
     cat ${WORKSPACE_MESON_OPTIONS_INDICATOR} \
     > meson_options.txt
@@ -35,6 +55,8 @@ then
     echo "option( 'sources', type : 'array', value : [ ${SOURCES} ] )" \
     >> meson_options.txt
     echo "option( 'x_cpp_flags_default', type : 'array', value : [ ${CPP_FLAGS_DEFAULT} ] )" \
+    >> meson_options.txt
+    echo "option( 'x_cpp_flags_focus', type : 'array', value : [ ${CPP_FLAGS_FOCUS} ] )" \
     >> meson_options.txt
 
     meson ${BUILD_DIR}
