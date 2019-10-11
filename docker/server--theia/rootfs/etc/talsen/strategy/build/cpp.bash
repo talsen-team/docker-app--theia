52b974b85d752158d9fc6cbe8d39f64551491396
David Lechner <david.lechner@talsen.team>
2019-10-11 11:52:52 +0200

    add customizable default cpp flags

--- docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
+++ docker/server--theia/rootfs/etc/talsen/strategy/build/cpp.bash
@@ -4,6 +4,7 @@ set -euo pipefail
 
 source /etc/talsen/util/indicator/workspace-gunit-lib-indicator.bash
 source /etc/talsen/util/indicator/workspace-meson-options-indicator.bash
+WORKSPACE_CPP_FLAGS_DEFAULT_INDICATOR=.workspace/.cpp-flags.default
 
 BUILD_DIR=.build
 PWD=$( pwd )
@@ -11,11 +12,19 @@ TARGET_NAME=run-workspace
 
 if [ ! -d ${BUILD_DIR} ];
 then
+    CPP_FLAGS_DEFAULT=""
+    for FLAG in $( cat ${WORKSPACE_CPP_FLAGS_DEFAULT_INDICATOR} )
+    do
+        CPP_FLAGS_DEFAULT="'${FLAG}', ${CPP_FLAGS_DEFAULT}"
+    done
+    CPP_FLAGS_DEFAULT="${CPP_FLAGS_DEFAULT::-2}"
+
     SOURCES=""
     for SOURCE in $( find . -name *.cpp -printf '%P ' | xargs echo )
     do
         SOURCES="'${SOURCE}', ${SOURCES}"
     done
+    SOURCES="${SOURCES::-2}"
 
     cat ${WORKSPACE_MESON_OPTIONS_INDICATOR} \
     > meson_options.txt
@@ -24,6 +33,8 @@ then
     echo "option( 'gunit_lib_dir', type : 'string', value : '$( pwd )/${WORKSPACE_GUNIT_LIB_INDICATOR}' )" \
     >> meson_options.txt
     echo "option( 'sources', type : 'array', value : [ ${SOURCES} ] )" \
+    >> meson_options.txt
+    echo "option( 'x_cpp_flags_default', type : 'array', value : [ ${CPP_FLAGS_DEFAULT} ] )" \
     >> meson_options.txt
 
     meson ${BUILD_DIR}
