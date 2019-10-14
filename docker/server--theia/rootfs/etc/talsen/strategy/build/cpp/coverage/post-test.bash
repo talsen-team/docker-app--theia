62bd689dafac7f0319dbf15f54c3ab17bcd88863
David Lechner <david.lechner@talsen.team>
2019-10-14 16:52:36 +0200

    generate coverage report for cpp

--- docker/server--theia/rootfs/etc/talsen/strategy/build/cpp/coverage/post-test.bash
+++ docker/server--theia/rootfs/etc/talsen/strategy/build/cpp/coverage/post-test.bash
@@ -2,11 +2,21 @@
 
 set -euo pipefail
 
+GLOBIGNORE="*" # prevent expansion of wildcards link '/usr/include/*' or similar
+
 BUILD_DIR=.build
-TEMP_COVERAGE_FILE=${BUILD_DIR}/meson-logs/coverage-temp.info
+COVERAGE_DIR=${BUILD_DIR}/coveragereport
+COVERAGE_FILE=${BUILD_DIR}/meson-logs/coverage.info
 TEMP_COVERAGE_DIR=${BUILD_DIR}/meson-logs/coveragereport
+TEMP_COVERAGE_FILE=${BUILD_DIR}/meson-logs/coverage-temp.info
 
 echo -e "Generating coverage report ..."
+
+LCOV_EXLCUDE=""
+while IFS= read -r ITEM
+do
+    LCOV_EXLCUDE="${LCOV_EXLCUDE}'${ITEM}' "
+done < .lcovignore
 
 lcov --directory ${BUILD_DIR}            \
      --capture                           \
@@ -16,11 +26,22 @@ lcov --directory ${BUILD_DIR}            \
 
 echo ""
 
+lcov --remove ${TEMP_COVERAGE_FILE}      \
+     $( eval echo -e "${LCOV_EXLCUDE}" ) \
+     --output-file ${COVERAGE_FILE}      \
+     --rc lcov_branch_coverage=1
+
+echo ""
+
 genhtml --prefix ${BUILD_DIR}                   \
         --output-directory ${TEMP_COVERAGE_DIR} \
         --title 'Code coverage'                 \
         --legend                                \
         --branch-coverage                       \
-        --show-details ${TEMP_COVERAGE_FILE}
+        --show-details ${COVERAGE_FILE}
+
+rsync --archive             \
+      ${TEMP_COVERAGE_DIR}/ \
+      ${COVERAGE_DIR}
 
 echo -e "Generating coverage report ... done"
