a2369ba0f1a0063e38348619bffe5d07c5f0e0fb
David Lechner <david.lechner@talsen.team>
2019-10-15 00:46:48 +0200

    generate cccc report on coverage focus

--- docker/server--theia/rootfs/etc/talsen/strategy/build/cpp/coverage/post-test.bash
+++ docker/server--theia/rootfs/etc/talsen/strategy/build/cpp/coverage/post-test.bash
@@ -2,15 +2,20 @@
 
 set -euo pipefail
 
-GLOBIGNORE="*" # prevent expansion of wildcards link '/usr/include/*' or similar
-
 BUILD_DIR=.build
+CCCC_DIR=.cccc
+CCCC_REPORT=${CCCC_DIR}/cccc.html
 COVERAGE_DIR=.coverage
 COVERAGE_FILE=${BUILD_DIR}/meson-logs/coverage.info
+COVERAGE_REPORT=${COVERAGE_DIR}/index.html
+PROJECT_DIR=$( pwd )
+SOURCE_DIR=src
 TEMP_COVERAGE_DIR=${BUILD_DIR}/meson-logs/coveragereport
 TEMP_COVERAGE_FILE=${BUILD_DIR}/meson-logs/coverage-temp.info
 
 echo -e "Generating coverage report ..."
+
+GLOBIGNORE="*" # prevent expansion of wildcards link '/usr/include/*' or similar
 
 LCOV_EXLCUDE=""
 while IFS= read -r ITEM
@@ -45,3 +50,28 @@ rsync --archive             \
       ${COVERAGE_DIR}
 
 echo -e "Generating coverage report ... done"
+
+echo -e "Generating \"C and C++ Code Counter\" report ..."
+
+unset GLOBIGNORE
+
+cd ${SOURCE_DIR}
+
+cccc *.cpp --outdir=${PROJECT_DIR}/${BUILD_DIR}/${CCCC_DIR} &> /dev/null
+
+rsync --archive                               \
+      ${PROJECT_DIR}/${BUILD_DIR}/${CCCC_DIR} \
+      ${PROJECT_DIR}
+
+rm --force --recursive                     \
+   ${PROJECT_DIR}/${BUILD_DIR}/${CCCC_DIR}
+
+cd ${PROJECT_DIR}
+
+echo -e "Generating \"C and C++ Code Counter\" report ... done"
+
+echo ""
+
+echo "--> To view the CCCC Report open \"${CCCC_REPORT}\" in preview mode."
+echo "--> To view the Coverage Report open \"${COVERAGE_REPORT}\" in preview mode."
+
