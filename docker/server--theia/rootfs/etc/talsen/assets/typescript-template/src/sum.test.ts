a2a012dcaefe3b3dae484c1dab184b41aadf1bbe
David Lechner <david.lechner@talsen.team>
2019-10-22 10:25:33 +0200

    add typescript template with all relevant commands

---
+++ docker/server--theia/rootfs/etc/talsen/assets/typescript-template/src/sum.test.ts
@@ -0,0 +1,4 @@
+import {sum} from "./sum";
+test("Example Sum Test", () => {
+    expect(sum(1, 2)).toBe(3);
+});
---
+++ docker/server--theia/rootfs/etc/talsen/strategy/init/typescript.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source /etc/talsen/util/indicator/workspace-template.bash
+
+ASSET_TEMPLATE=/etc/talsen/assets/typescript-template
+
+rsync --archive            \
+        ${ASSET_TEMPLATE}/ \
+        .
+
+echo "--> Workspace has been initialized with \"$( cat ${WORKSPACE_TEMPLATE_INDICATOR} )\" template."
