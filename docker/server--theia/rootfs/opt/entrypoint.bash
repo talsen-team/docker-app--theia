ffe927f3bf05685cb2a949ce752120fa75bb4ba5
David Lechner <david.lechner@talsen.team>
2019-10-31 10:45:41 +0100

    attempt to fix permissions on theia settings file

--- docker/server--theia/rootfs/opt/entrypoint.bash
+++ docker/server--theia/rootfs/opt/entrypoint.bash
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-THEIA_CONF_DIR=/home/theia/.theia
+HOME_DIR=/home/theia
 PROJECT_DIR=/home/project
 PLUGINS_DIR=/home/theia/plugins
 
-sudo chown --recursive       \
-           theia:theia       \
-           ${THEIA_CONF_DIR}
+sudo chown --recursive \
+           theia:theia \
+           ${HOME_DIR}
 
 sudo chown --recursive    \
            theia:theia    \
