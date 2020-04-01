a909c49aca83ffcf03a2502fec72b3864381f9e4
David Lechner <david.lechner@talsen.team>
2020-04-01 23:26:08 +0200

    persist user specific theia directory

--- docker/server--theia/rootfs/opt/entrypoint.bash
+++ docker/server--theia/rootfs/opt/entrypoint.bash
@@ -3,11 +3,16 @@
 set -euo pipefail
 
 PROJECT_DIR=/home/project
+THEIA_DIR=/home/theia/.theia
 PLUGINS_DIR=/home/theia/plugins
 
 sudo chown --recursive    \
            theia:theia    \
            ${PROJECT_DIR}
+
+sudo chown --recursive    \
+           theia:theia    \
+           ${THEIA_DIR}
 
 yarn theia start ${PROJECT_DIR}                     \
                  --hostname=0.0.0.0                 \
