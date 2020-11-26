2a9f6098b5525946d70955a932116a1b057b55bb
David Lechner <david.lechner@talsen.team>
2020-11-26 13:40:22 +0100

    hide broken test coverage

--- docker/server--theia/rootfs/etc/talsen/strategy/build/csharp.bash
+++ docker/server--theia/rootfs/etc/talsen/strategy/build/csharp.bash
@@ -8,15 +8,11 @@ then
     exit 1
 fi
 
-echo "--> The terminal may hang for a few seconds until the build is done."
-
 set +e
 
 mkdir --parents \
       .build
 
-dotnet test /p:CollectCoverage=true &> .build/.test-output
-
-cat .build/.test-output
+dotnet test
 
 set -e
